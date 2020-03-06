e9ebcfd1b76b37de1817bfa4d36b5fe24fb9687e
DESKTOP-ULQFVU9\Sims <rpavlenko.mail@gmail.com>
2020-03-06 22:30:58 +0100

    added browser-sync and mininfy in gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 const gulp = require('gulp');
-var browserSync = require('browser-sync').create();
+const browserSync = require('browser-sync').create();
+const cleanCSS = require('gulp-clean-css');
+const rename = require('gulp-rename');
 
 gulp.task('hello', function(done){
   console.log('hello');
@@ -14,4 +16,16 @@ gulp.task('browser-sync', function () {
   });
 
   gulp.watch("./*.html").on('change', browserSync.reload);
+  gulp.watch("./src/css/*.css").on('change', browserSync.reload);
+});
+
+gulp.task('minify', () => {
+  return gulp.src('./src/css/*.css')
+    .pipe(cleanCSS({
+      compatibility: 'ie8'
+    }))
+      .pipe(rename({
+        suffix: '.min'
+      }))
+    .pipe(gulp.dest('./src/css'));
 });
