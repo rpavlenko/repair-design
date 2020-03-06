7186865377546946fa787d698a34b79400eebca3
DESKTOP-ULQFVU9\Sims <rpavlenko.mail@gmail.com>
2020-03-06 22:13:44 +0100

    include gulp & make gulp.js

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+const gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+
+gulp.task('hello', function(done){
+  console.log('hello');
+  done();
+});
+
+gulp.task('browser-sync', function () {
+  browserSync.init({
+    server: {
+      baseDir: "./"
+    }
+  });
+
+  gulp.watch("./*.html").on('change', browserSync.reload);
+});
