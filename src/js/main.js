47aa73cdbe51c925bd449bcfdfd0e441dde21ed9
DESKTOP-ULQFVU9\Sims <rpavlenko.mail@gmail.com>
2020-03-12 10:00:42 +0100

    feat: git modal close on click out of modal & Escape pressed

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,31 @@
 document.addEventListener("DOMContentLoaded", function (event) {
-    const modal = document.querySelector('.modal');
-    const modalBtn = document.querySelectorAll('[data-toggle=modal]');
-    const closeBtn = document.querySelector('.modal__close') ;
+  const modal = document.querySelector('.modal');
+  const modalBtn = document.querySelectorAll('[data-toggle=modal]');
+  const closeBtn = document.querySelector('.modal__close');
 
-    const switchModal = () => {
-      modal.classList.toggle('modal--visible');
-    }
+
+  const switchModal = () => {
+    modal.classList.toggle('modal--visible');
+  }
     
-    modalBtn.forEach(element => {
-      element.addEventListener('click', switchModal);
-    });
+  modalBtn.forEach(element => {
+    element.addEventListener('click', switchModal);
+  });
 
-    closeBtn.addEventListener('click', switchModal);
+  closeBtn.addEventListener('click', switchModal);
+
+  // close modal when click out of modal
+  window.onclick = function (event) {
+    if (event.target == modal) {
+      switchModal();
+    }
+  }
+  
+  // close modal when Escapу pressed
+  window.addEventListener('keydown', function (event) {
+    if (event.key === 'Escape') {
+      modal.classList.remove('modal--visible')
+    }
+  })
+
 });
