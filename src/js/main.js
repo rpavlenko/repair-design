b5e8eef43519159062cd21adbc93a640944f6690
DESKTOP-ULQFVU9\Sims <rpavlenko.mail@gmail.com>
2020-03-13 22:32:56 +0100

    feat: change to jquery close on modal & add scroll up button

--- src/js/main.js
+++ src/js/main.js
@@ -1,31 +1,59 @@
-document.addEventListener("DOMContentLoaded", function (event) {
-  const modal = document.querySelector('.modal');
-  const modalBtn = document.querySelectorAll('[data-toggle=modal]');
-  const closeBtn = document.querySelector('.modal__close');
+// document.addEventListener("DOMContentLoaded", function (event) {
+//   const modal = document.querySelector('.modal');
+//   const modalBtn = document.querySelectorAll('[data-toggle=modal]');
+//   const closeBtn = document.querySelector('.modal__close');
 
 
-  const switchModal = () => {
-    modal.classList.toggle('modal--visible');
-  }
+//   const switchModal = () => {
+//     modal.classList.toggle('modal--visible');
+//   }
     
-  modalBtn.forEach(element => {
-    element.addEventListener('click', switchModal);
+//   modalBtn.forEach(element => {
+//     element.addEventListener('click', switchModal);
+//   });
+
+//   closeBtn.addEventListener('click', switchModal);
+
+//   // close modal when click out of modal
+//   window.onclick = function (event) {
+//     if (event.target == modal) {
+//       switchModal();
+//     }
+//   }
+  
+//   // close modal when Escape pressed
+//   window.addEventListener('keydown', function (event) {
+//     if (event.key === 'Escape') {
+//       modal.classList.remove('modal--visible')
+//     }
+//   })
+
+// });
+
+$(document).ready(function () {
+  var modal = $('.modal'),
+      modalBtn = $('[data-toggle=modal]');
+      closeBtn = $('.modal__close');
+  modalBtn.on('click', function(){
+    modal.toggleClass('modal--visible');
+  });
+  closeBtn.on('click', function(){
+    modal.toggleClass('modal--visible');
   });
 
-  closeBtn.addEventListener('click', switchModal);
+})
 
-  // close modal when click out of modal
-  window.onclick = function (event) {
-    if (event.target == modal) {
-      switchModal();
-    }
+$("#button").on("click", function () {
+  $("html").animate({
+    scrollTop: 0
+  }, "slow");
+});
+
+var btn = $('#button');
+$(window).scroll(function () {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
   }
-  
-  // close modal when Escape pressed
-  window.addEventListener('keydown', function (event) {
-    if (event.key === 'Escape') {
-      modal.classList.remove('modal--visible')
-    }
-  })
-
 });
