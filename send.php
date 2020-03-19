e2b14d1e9df9d7fcc78bfdde83b5e5ded1e0cd32
DESKTOP-ULQFVU9\Sims <rpavlenko.mail@gmail.com>
2020-03-19 18:52:31 +0100

    feat: add phpmailer to site

---
+++ send.php
@@ -0,0 +1,45 @@
+<?php
+
+  $userName = $_POST['userName'];
+  $userEmail = $_POST['userEmail'];
+  $userPhone = $_POST['userPhone'];
+  $userQuestion = $_POST['userQuestion'];
+
+// Load Composer's autoloader
+
+require 'phpmailer/Exception.php';
+require 'phpmailer/PHPMailer.php';
+require 'phpmailer/SMTP.php';
+
+
+// Instantiation and passing `true` enables exceptions
+$mail = new PHPMailer\PHPMailer\PHPMailer();
+
+try {
+    //Server settings
+    $mail->SMTPDebug = 0;                      // Enable verbose debug output
+    $mail->isSMTP();                                            // Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+    $mail->Username   = 'repairdesigntestsite@gmail.com';                     // SMTP username
+    $mail->Password   = 'a9vHrJ{+}n5L6J#}';                               // SMTP password
+    $mail->SMTPSecure = 'ssl';         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+    $mail->Port       = 465;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+
+    //Recipients
+    $mail->setFrom('repairdesigntestsite@gmail.com', 'Роман');
+    $mail->addAddress('WebStartfourmap@yandex.ru');     // Add a recipient
+
+    // Attachments
+
+    // Content
+    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = 'Новая заявка с сайта';
+    $mail->Body    = "Имя пользователя: ${userName}, его телефон: ${userPhone}, его почта ${userEmail}.
+    Его вопрос: ${userQuestion}";
+
+    $mail->send();
+    header('Location: thanks.html');
+} catch (Exception $e) {
+    echo "Письмо не отправлено, есть ошибка. Код ошибки: {$mail->ErrorInfo}";
+}
