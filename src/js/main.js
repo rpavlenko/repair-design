0800ef6040b3011cecd4bb8b976c6519cec32bf0
DESKTOP-ULQFVU9\Sims <rpavlenko.mail@gmail.com>
2020-03-21 21:23:23 +0100

    feat: delete yandex maps & added google map

--- src/js/main.js
+++ src/js/main.js
@@ -333,43 +333,5 @@ $(document).ready(function () {
   // WOW animation
   new WOW().init();
 
-
-  // Yandex Map
-  ymaps.ready(function () {
-    var myMap = new ymaps.Map('map', {
-        center: [55.786774, 49.142715],
-        zoom: 9
-      }, {
-        searchControlProvider: 'yandex#search'
-      }),
-
-      // Создаём макет содержимого.
-      MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
-        '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
-      ),
-
-      myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
-        hintContent: 'Наш офис',
-        balloonContent: 'Вход со двора'
-      }, {
-        // Опции.
-        // Необходимо указать данный тип макета.
-        iconLayout: 'default#image',
-        // Своё изображение иконки метки.
-        iconImageHref: 'src/img/marker.png',
-        // Размеры метки.
-        iconImageSize: [32, 32],
-        // Смещение левого верхнего угла иконки относительно
-        // её "ножки" (точки привязки).
-        iconImageOffset: [-5, -38]
-      });
-
-    myMap.behaviors.disable('scrollZoom');
-    myMap.geoObjects
-      .add(myPlacemark)
-  });
-
-
-
 });
 
